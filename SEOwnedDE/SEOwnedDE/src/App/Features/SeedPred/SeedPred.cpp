b06b82ad6dc4d2fce6ec728c2d16a688086e3977
lnx00 <34384416+lnx00@users.noreply.github.com>
2023-12-05 01:58:57 +0100

    SeedPres: Turned variables into class members

--- SEOwnedDE/SEOwnedDE/src/App/Features/SeedPred/SeedPred.cpp
+++ SEOwnedDE/SEOwnedDE/src/App/Features/SeedPred/SeedPred.cpp
@@ -2,22 +2,11 @@
 
 #include "../CFG.h"
 
-bool synced{false};
-float server_time{0.0f};
-float prev_server_time{0.0f};
-float ask_time{0.0f};
-float guess_time{0.0f};
-float sync_offset{0.0f};
-bool waiting_for_pp{false};
-float guess_delta{0.0f};
-float response_time{0.0f};
-
 void CSeedPred::AskForPlayerPerf()
 {
 	if (!CFG::Exploits_SeedPred_Active)
 	{
 		Reset();
-
 		return;
 	}
 
@@ -25,7 +14,6 @@ void CSeedPred::AskForPlayerPerf()
 	if (!weapon || !(weapon->GetDamageType() & DMG_BULLET) || H::AimUtils->GetWeaponType(weapon) != EWeaponType::HITSCAN || weapon->GetWeaponSpread() <= 0.0f)
 	{
 		Reset();
-
 		return;
 	}
 
@@ -34,21 +22,20 @@ void CSeedPred::AskForPlayerPerf()
 		if (local->deadflag())
 		{
 			Reset();
-
 			return;
 		}
 	}
 
-	if (waiting_for_pp)
+	if (m_WaitingForPP)
 	{
 		return;
 	}
 
 	I::ClientState->SendStringCmd("playerperf");
 
-	ask_time = static_cast<float>(Plat_FloatTime());
+	m_AskTime = static_cast<float>(Plat_FloatTime());
 
-	waiting_for_pp = true;
+	m_WaitingForPP = true;
 }
 
 bool CSeedPred::ParsePlayerPerf(bf_read& msgData)
@@ -73,39 +60,39 @@ bool CSeedPred::ParsePlayerPerf(bf_read& msgData)
 
 	if (matches.size() == 2)
 	{
-		waiting_for_pp = false;
+		m_WaitingForPP = false;
 
 		//credits to kgb for idea
 
 		const float newServerTime{std::stof(matches[1].str())};
 
-		if (newServerTime > server_time)
+		if (newServerTime > m_ServerTime)
 		{
-			prev_server_time = server_time;
+			m_PrevServerTime = m_ServerTime;
 
-			server_time = newServerTime;
+			m_ServerTime = newServerTime;
 
-			response_time = static_cast<float>(Plat_FloatTime() - ask_time);
+			m_ResponseTime = static_cast<float>(Plat_FloatTime() - m_AskTime);
 
-			//if (!synced)
+			//if (!m_Synced)
 			{
-				if (prev_server_time > 0.0f)
+				if (m_PrevServerTime > 0.0f)
 				{
-					if (guess_time > 0.0f)
+					if (m_GuessTime > 0.0f)
 					{
-						const float delta{server_time - guess_time};
+						const float delta{m_ServerTime - m_GuessTime};
 
 						if (delta == 0.0f)
 						{
-							synced = true;
+							m_Synced = true;
 
-							sync_offset = guess_delta;
+							m_SyncOffset = m_GuessDelta;
 						}
 					}
 
-					guess_delta = server_time - prev_server_time;
+					m_GuessDelta = m_ServerTime - m_PrevServerTime;
 
-					guess_time = server_time + (guess_delta);
+					m_GuessTime = m_ServerTime + (m_GuessDelta);
 				}
 			}
 		}
@@ -118,27 +105,27 @@ bool CSeedPred::ParsePlayerPerf(bf_read& msgData)
 
 int CSeedPred::GetSeed()
 {
-	float time{(server_time + sync_offset + response_time) * 1000.0f};
+	float time{(m_ServerTime + m_SyncOffset + m_ResponseTime) * 1000.0f};
 
 	return *reinterpret_cast<int*>(reinterpret_cast<char*>(&time)) & 255;
 }
 
 void CSeedPred::Reset()
 {
-	synced = false;
-	server_time = 0.0f;
-	prev_server_time = 0.0f;
-	ask_time = 0.0f;
-	guess_time = 0.0f;
-	sync_offset = 0.0f;
-	waiting_for_pp = false;
-	guess_delta = 0.0f;
-	response_time = 0.0f;
+	m_Synced = false;
+	m_ServerTime = 0.0f;
+	m_PrevServerTime = 0.0f;
+	m_AskTime = 0.0f;
+	m_GuessTime = 0.0f;
+	m_SyncOffset = 0.0f;
+	m_WaitingForPP = false;
+	m_GuessDelta = 0.0f;
+	m_ResponseTime = 0.0f;
 }
 
 void CSeedPred::AdjustAngles(CUserCmd* cmd)
 {
-	if (!CFG::Exploits_SeedPred_Active || !synced || !cmd || !G::bFiring)
+	if (!CFG::Exploits_SeedPred_Active || !m_Synced || !cmd || !G::bFiring)
 	{
 		return;
 	}
@@ -162,15 +149,12 @@ void CSeedPred::AdjustAngles(CUserCmd* cmd)
 	}
 
 	auto bulletsPerShot{weapon->GetWeaponInfo()->GetWeaponData(TF_WEAPON_PRIMARY_MODE).m_nBulletsPerShot};
-
 	bulletsPerShot = static_cast<int>(SDKUtils::AttribHookValue(static_cast<float>(bulletsPerShot), "mult_bullets_per_shot", weapon));
 
 	//credits to cathook for average spread stuff
 
 	std::vector<Vec3> bulletCorrections{};
-
 	Vec3 averageSpread{};
-
 	auto seed{GetSeed()};
 
 	for (auto bullet{0}; bullet < bulletsPerShot; bullet++)
@@ -218,22 +202,19 @@ void CSeedPred::AdjustAngles(CUserCmd* cmd)
 
 	Vec3 fixedSpread{FLT_MAX, FLT_MAX, FLT_MAX};
 
-	for (const auto& spread : bulletCorrections)
+	for (const auto& curSpread : bulletCorrections)
 	{
-		if (spread.DistTo(averageSpread) < fixedSpread.DistTo(averageSpread))
+		if (curSpread.DistTo(averageSpread) < fixedSpread.DistTo(averageSpread))
 		{
-			fixedSpread = spread;
+			fixedSpread = curSpread;
 		}
 	}
 
 	Vec3 fixedAngles{};
-
 	Math::VectorAngles(fixedSpread, fixedAngles);
 
 	const Vec3 correction{cmd->viewangles - fixedAngles};
-
 	cmd->viewangles += correction;
-
 	Math::ClampAngles(cmd->viewangles);
 
 	G::bSilentAngles = true;
@@ -241,7 +222,7 @@ void CSeedPred::AdjustAngles(CUserCmd* cmd)
 
 void CSeedPred::Paint()
 {
-	if (!CFG::Exploits_SeedPred_Active || I::EngineVGui->IsGameUIVisible() || SDKUtils::BInEndOfMatch() || server_time <= 0.0f)
+	if (!CFG::Exploits_SeedPred_Active || I::EngineVGui->IsGameUIVisible() || SDKUtils::BInEndOfMatch() || m_ServerTime <= 0.0f)
 	{
 		return;
 	}
@@ -255,10 +236,10 @@ void CSeedPred::Paint()
 	{
 		auto calcMantissaStep = [](float val)
 		{
-			auto raw_val{reinterpret_cast<int&>(val)};
-			auto exponent{(raw_val >> 23) & 0xFF};
+			const auto rawVal{reinterpret_cast<int&>(val)};
+			const auto exponent{(rawVal >> 23) & 0xFF};
 
-			auto result{powf(2.0f, static_cast<float>(exponent - (127 + 23))) * 1000.0f};
+			const auto result{powf(2.0f, static_cast<float>(exponent - (127 + 23))) * 1000.0f};
 
 			static std::vector<float> mantissas{};
 
@@ -276,8 +257,7 @@ void CSeedPred::Paint()
 
 			auto closest = [](const std::vector<float>& vec, float value)
 			{
-				auto it{std::lower_bound(vec.begin(), vec.end(), value)};
-
+				const auto it{std::ranges::lower_bound(vec, value)};
 				if (it == vec.end())
 				{
 					return value;
@@ -289,7 +269,7 @@ void CSeedPred::Paint()
 			return closest(mantissas, result);
 		};
 
-		std::chrono::hh_mm_ss time{std::chrono::seconds(static_cast<int>(server_time))};
+		const std::chrono::hh_mm_ss time{std::chrono::seconds(static_cast<int>(m_ServerTime))};
 
 		int x{2};
 		int y{2};
@@ -299,7 +279,7 @@ void CSeedPred::Paint()
 			H::Fonts->Get(EFonts::ESP_SMALL),
 			x, y,
 			{200, 200, 200, 255}, POS_DEFAULT,
-			std::format("{}h {}m {}s (step {:.0f})", time.hours().count(), time.minutes().count(), time.seconds().count(), calcMantissaStep(server_time)).c_str()
+			std::format("{}h {}m {}s (step {:.0f})", time.hours().count(), time.minutes().count(), time.seconds().count(), calcMantissaStep(m_ServerTime)).c_str()
 		);
 
 		y += 10;
@@ -308,8 +288,8 @@ void CSeedPred::Paint()
 		(
 			H::Fonts->Get(EFonts::ESP_SMALL),
 			x, y,
-			!synced ? Color_t{250, 130, 49, 255} : Color_t{32, 191, 107, 255}, POS_DEFAULT,
-			!synced ? "syncing.." : std::format("synced ({})", sync_offset).c_str()
+			!m_Synced ? Color_t{250, 130, 49, 255} : Color_t{32, 191, 107, 255}, POS_DEFAULT,
+			!m_Synced ? "syncing.." : std::format("synced ({})", m_SyncOffset).c_str()
 		);
 	}
 }
--- SEOwnedDE/SEOwnedDE/src/App/Features/SeedPred/SeedPred.h
+++ SEOwnedDE/SEOwnedDE/src/App/Features/SeedPred/SeedPred.h
@@ -4,6 +4,16 @@
 
 class CSeedPred
 {
+	bool m_Synced{false};
+	float m_ServerTime{0.0f};
+	float m_PrevServerTime{0.0f};
+	float m_AskTime{0.0f};
+	float m_GuessTime{0.0f};
+	float m_SyncOffset{0.0f};
+	bool m_WaitingForPP{false};
+	float m_GuessDelta{0.0f};
+	float m_ResponseTime{0.0f};
+
 public:
 	void AskForPlayerPerf();
 	bool ParsePlayerPerf(bf_read& msgData);
