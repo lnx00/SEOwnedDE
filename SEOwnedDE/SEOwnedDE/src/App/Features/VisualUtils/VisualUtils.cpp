e1a5415198fd52eb9f38e0372413c8a7bb08a088
lnx00 <34384416+lnx00@users.noreply.github.com>
2023-12-07 01:36:12 +0100

    Improved VisualUtils code consistency

--- SEOwnedDE/SEOwnedDE/src/App/Features/VisualUtils/VisualUtils.cpp
+++ SEOwnedDE/SEOwnedDE/src/App/Features/VisualUtils/VisualUtils.cpp
@@ -6,39 +6,41 @@
 
 #include "../Players/Players.h"
 
-bool CVisualUtils::IsEntityOwnedBy(C_BaseEntity *pEntity, C_BaseEntity *pWho)
+bool CVisualUtils::IsEntityOwnedBy(C_BaseEntity* pEntity, C_BaseEntity* pWho)
 {
 	switch (pEntity->GetClassId())
 	{
-		case ETFClassIds::CTFGrenadePipebombProjectile:
-		case ETFClassIds::CTFProjectile_Jar:
-		case ETFClassIds::CTFProjectile_JarGas:
-		case ETFClassIds::CTFProjectile_JarMilk:
-		case ETFClassIds::CTFProjectile_Cleaver: {
+	case ETFClassIds::CTFGrenadePipebombProjectile:
+	case ETFClassIds::CTFProjectile_Jar:
+	case ETFClassIds::CTFProjectile_JarGas:
+	case ETFClassIds::CTFProjectile_JarMilk:
+	case ETFClassIds::CTFProjectile_Cleaver:
+		{
 			return pEntity->As<C_BaseGrenade>()->m_hThrower().Get() == pWho;
 		}
 
-		case ETFClassIds::CTFProjectile_SentryRocket:
+	case ETFClassIds::CTFProjectile_SentryRocket:
 		{
-			if (auto pOwner = pEntity->m_hOwnerEntity().Get())
+			if (const auto pOwner = pEntity->m_hOwnerEntity().Get())
 				return pOwner->As<C_BaseObject>()->m_hBuilder() == pWho;
 
 			break;
 		}
 
-		case ETFClassIds::CObjectSentrygun:
-		case ETFClassIds::CObjectDispenser:
-		case ETFClassIds::CObjectTeleporter: {
+	case ETFClassIds::CObjectSentrygun:
+	case ETFClassIds::CObjectDispenser:
+	case ETFClassIds::CObjectTeleporter:
+		{
 			return pEntity->As<C_BaseObject>()->m_hBuilder() == pWho;
 		}
 
-		default: return pEntity->m_hOwnerEntity().Get() == pWho;
+	default: return pEntity->m_hOwnerEntity().Get() == pWho;
 	}
 
 	return false;
 }
 
-Color_t CVisualUtils::GetEntityColor(C_TFPlayer *pLocal, C_BaseEntity *pEntity)
+Color_t CVisualUtils::GetEntityColor(C_TFPlayer* pLocal, C_BaseEntity* pEntity)
 {
 	if (!pLocal || !pEntity)
 		return { 255, 255, 255, 255 };
@@ -48,7 +50,7 @@ Color_t CVisualUtils::GetEntityColor(C_TFPlayer *pLocal, C_BaseEntity *pEntity)
 
 	if (pEntity->GetClassId() == ETFClassIds::CTFPlayer)
 	{
-		auto pPlayer = pEntity->As<C_TFPlayer>();
+		const auto pPlayer = pEntity->As<C_TFPlayer>();
 
 		if (pPlayer->IsInvulnerable())
 			return CFG::Color_Invulnerable;
@@ -61,8 +63,8 @@ Color_t CVisualUtils::GetEntityColor(C_TFPlayer *pLocal, C_BaseEntity *pEntity)
 
 		if (pPlayer != pLocal)
 		{
+			// TODO: Handle these colors in F::Players
 			PlayerPriority info{};
-
 			F::Players->GetInfo(pPlayer->entindex(), info);
 
 			if (info.Cheater)
@@ -95,29 +97,29 @@ Color_t CVisualUtils::GetHealthColor(int nHealth, int nMaxHealth)
 		return CFG::Color_OverHeal;
 
 	nHealth = std::max(0, std::min(nHealth, nMaxHealth));
-	int r = std::min((510 * (nMaxHealth - nHealth)) / nMaxHealth, 210);
-	int g = std::min((510 * nHealth) / nMaxHealth, 230);
+	const int r = std::min((510 * (nMaxHealth - nHealth)) / nMaxHealth, 210);
+	const int g = std::min((510 * nHealth) / nMaxHealth, 230);
 	return { static_cast<byte>(r), static_cast<byte>(g), 50, 255 };
 }
 
 Color_t CVisualUtils::GetHealthColorAlt(int nHealth, int nMaxHealth)
 {
 	nHealth = std::max(0, std::min(nHealth, nMaxHealth));
-	int r = std::min((510 * (nMaxHealth - nHealth)) / nMaxHealth, 255);
-	int g = std::min((510 * nHealth) / nMaxHealth, 230);
+	const int r = std::min((510 * (nMaxHealth - nHealth)) / nMaxHealth, 255);
+	const int g = std::min((510 * nHealth) / nMaxHealth, 230);
 	return { static_cast<byte>(r), static_cast<byte>(g), 50, 255 };
 }
 
-int CVisualUtils::CreateTextureFromArray(const unsigned char *rgba, int w, int h)
+int CVisualUtils::CreateTextureFromArray(const unsigned char* rgba, int w, int h)
 {
-	int nTextureIdOut = I::MatSystemSurface->CreateNewTextureID(true);	
+	const int nTextureIdOut = I::MatSystemSurface->CreateNewTextureID(true);
 	I::MatSystemSurface->DrawSetTextureRGBAEx(nTextureIdOut, rgba, w, h, IMAGE_FORMAT_BGRA8888);
 	return nTextureIdOut;
 }
 
-int CVisualUtils::CreateTextureFromVTF(const char *name)
+int CVisualUtils::CreateTextureFromVTF(const char* name)
 {
-	int nTextureIdOut = I::MatSystemSurface->CreateNewTextureID(false);
+	const int nTextureIdOut = I::MatSystemSurface->CreateNewTextureID(false);
 	I::MatSystemSurface->DrawSetTextureFile(nTextureIdOut, name, 0, true);
 	return nTextureIdOut;
 }
@@ -125,6 +127,7 @@ int CVisualUtils::CreateTextureFromVTF(const char *name)
 int CVisualUtils::GetClassIcon(int nClassNum)
 {
 	//what are arrays
+	// TODO: Use the game's textures
 
 	static int nScout = CreateTextureFromArray(Icons::leaderboard_class_scout, 64, 64);
 	static int nSoldier = CreateTextureFromArray(Icons::leaderboard_class_soldier, 64, 64);
@@ -138,22 +141,22 @@ int CVisualUtils::GetClassIcon(int nClassNum)
 
 	switch (nClassNum)
 	{
-		case TF_CLASS_SCOUT: return nScout;
-		case TF_CLASS_SOLDIER: return nSoldier;
-		case TF_CLASS_PYRO: return nPyro;
-		case TF_CLASS_DEMOMAN: return nDemoman;
-		case TF_CLASS_HEAVYWEAPONS: return nHeavy;
-		case TF_CLASS_ENGINEER: return nEngineer;
-		case TF_CLASS_MEDIC: return nMedic;
-		case TF_CLASS_SNIPER: return nSniper;
-		case TF_CLASS_SPY: return nSpy;
-		default: break;
+	case TF_CLASS_SCOUT: return nScout;
+	case TF_CLASS_SOLDIER: return nSoldier;
+	case TF_CLASS_PYRO: return nPyro;
+	case TF_CLASS_DEMOMAN: return nDemoman;
+	case TF_CLASS_HEAVYWEAPONS: return nHeavy;
+	case TF_CLASS_ENGINEER: return nEngineer;
+	case TF_CLASS_MEDIC: return nMedic;
+	case TF_CLASS_SNIPER: return nSniper;
+	case TF_CLASS_SPY: return nSpy;
+	default: break;
 	}
 
 	return 0;
 }
 
-int CVisualUtils::GetBuildingTextureId(C_BaseObject *pObject)
+int CVisualUtils::GetBuildingTextureId(C_BaseObject* pObject)
 {
 	static int nSentryGunLvl1 = CreateTextureFromVTF("hud/hud_obj_status_sentry_1.vtf");
 	static int nSentryGunLvl2 = CreateTextureFromVTF("hud/hud_obj_status_sentry_2.vtf");
@@ -166,23 +169,23 @@ int CVisualUtils::GetBuildingTextureId(C_BaseObject *pObject)
 
 	switch (pObject->GetClassId())
 	{
-		case ETFClassIds::CObjectSentrygun:
+	case ETFClassIds::CObjectSentrygun:
 		{
 			switch (pObject->m_iUpgradeLevel())
 			{
-				case 1: return nSentryGunLvl1;
-				case 2: return nSentryGunLvl2;
-				case 3: return nSentryGunLvl3;
-				default: break;
+			case 1: return nSentryGunLvl1;
+			case 2: return nSentryGunLvl2;
+			case 3: return nSentryGunLvl3;
+			default: break;
 			}
 
 			break;
 		}
 
-		case ETFClassIds::CObjectDispenser: return nDispenser;
-		case ETFClassIds::CObjectTeleporter: return nTeleporter;
+	case ETFClassIds::CObjectDispenser: return nDispenser;
+	case ETFClassIds::CObjectTeleporter: return nTeleporter;
 
-		default: break;
+	default: break;
 	}
 
 	return 0;
@@ -206,10 +209,9 @@ int CVisualUtils::GetHalloweenGiftTextureId()
 	return nOut;
 }
 
-bool CVisualUtils::IsOnScreen(C_TFPlayer *pLocal, C_BaseEntity *pEntity)
+bool CVisualUtils::IsOnScreen(const C_TFPlayer* pLocal, const C_BaseEntity* pEntity)
 {
-	Vec3 vPos = pEntity->GetAbsOrigin();
-
+	const Vec3& vPos = pEntity->GetAbsOrigin();
 	if (vPos.DistTo(pLocal->GetAbsOrigin()) > 300.0f)
 	{
 		Vec3 vScreen = {};
@@ -223,16 +225,18 @@ bool CVisualUtils::IsOnScreen(C_TFPlayer *pLocal, C_BaseEntity *pEntity)
 				return false;
 		}
 
-		else return false;
+		else
+		{
+			return false;
+		}
 	}
 
 	return true;
 }
 
-bool CVisualUtils::IsOnScreenNoEntity(C_TFPlayer *pLocal, const Vec3 &vAbsOrigin)
+bool CVisualUtils::IsOnScreenNoEntity(const C_TFPlayer* pLocal, const Vec3& vAbsOrigin)
 {
-	Vec3 vPos = vAbsOrigin;
-
+	const Vec3& vPos = vAbsOrigin;
 	if (vPos.DistTo(pLocal->GetAbsOrigin()) > 300.0f)
 	{
 		Vec3 vScreen = {};
@@ -245,8 +249,10 @@ bool CVisualUtils::IsOnScreenNoEntity(C_TFPlayer *pLocal, const Vec3 &vAbsOrigin
 				|| vScreen.y > H::Draw->GetScreenH() + 400)
 				return false;
 		}
-
-		else return false;
+		else
+		{
+			return false;
+		}
 	}
 
 	return true;
@@ -256,8 +262,8 @@ int CVisualUtils::GetCat(int nFrame)
 {
 	if (nFrame < 0 || nFrame > 3)
 		return 0;
-	
-	static const int arrFrames[4] =
+
+	static const int ARR_FRAMES[4] =
 	{
 		CreateTextureFromArray(Icons::cat_0, 12, 12),
 		CreateTextureFromArray(Icons::cat_1, 12, 12),
@@ -265,7 +271,7 @@ int CVisualUtils::GetCat(int nFrame)
 		CreateTextureFromArray(Icons::cat_3, 12, 12)
 	};
 
-	return arrFrames[nFrame];
+	return ARR_FRAMES[nFrame];
 }
 
 int CVisualUtils::GetCat2(int nFrame)
@@ -273,7 +279,7 @@ int CVisualUtils::GetCat2(int nFrame)
 	if (nFrame < 0 || nFrame > 3)
 		return 0;
 
-	static const int arrFrames[4] =
+	static const int ARR_FRAMES[4] =
 	{
 		CreateTextureFromArray(Icons::cat2_0, 12, 12),
 		CreateTextureFromArray(Icons::cat2_1, 12, 12),
@@ -281,7 +287,7 @@ int CVisualUtils::GetCat2(int nFrame)
 		CreateTextureFromArray(Icons::cat2_3, 12, 12)
 	};
 
-	return arrFrames[nFrame];
+	return ARR_FRAMES[nFrame];
 }
 
 int CVisualUtils::GetCatSleep(int nFrame)
@@ -289,7 +295,7 @@ int CVisualUtils::GetCatSleep(int nFrame)
 	if (nFrame < 0 || nFrame > 3)
 		return 0;
 
-	static const int arrFrames[4] =
+	static const int ARR_FRAMES[4] =
 	{
 		CreateTextureFromArray(Icons::cat_sleep0, 16, 8),
 		CreateTextureFromArray(Icons::cat_sleep1, 16, 8),
@@ -297,7 +303,7 @@ int CVisualUtils::GetCatSleep(int nFrame)
 		CreateTextureFromArray(Icons::cat_sleep3, 16, 8)
 	};
 
-	return arrFrames[nFrame];
+	return ARR_FRAMES[nFrame];
 }
 
 int CVisualUtils::GetCatRun(int nFrame)
@@ -305,7 +311,7 @@ int CVisualUtils::GetCatRun(int nFrame)
 	if (nFrame < 0 || nFrame > 7)
 		return 0;
 
-	static const int arrFrames[8] =
+	static const int ARR_FRAMES[8] =
 	{
 		CreateTextureFromArray(Icons::cat_run0, 20, 13),
 		CreateTextureFromArray(Icons::cat_run1, 20, 13),
@@ -317,27 +323,27 @@ int CVisualUtils::GetCatRun(int nFrame)
 		CreateTextureFromArray(Icons::cat_run7, 20, 13)
 	};
 
-	return arrFrames[nFrame];
+	return ARR_FRAMES[nFrame];
 }
 
 Color_t CVisualUtils::Rainbow()
 {
-	float t = TICKS_TO_TIME(I::GlobalVars->tickcount);
-	
-	int r = static_cast<int>(std::round(std::cos(I::GlobalVars->realtime + t + 0.0f) * 127.5f + 127.5f));
-	int g = static_cast<int>(std::round(std::cos(I::GlobalVars->realtime + t + 2.0f) * 127.5f + 127.5f));
-	int b = static_cast<int>(std::round(std::cos(I::GlobalVars->realtime + t + 4.0f) * 127.5f + 127.5f));
+	const float t = TICKS_TO_TIME(I::GlobalVars->tickcount);
+
+	const int r = static_cast<int>(std::round(std::cos(I::GlobalVars->realtime + t + 0.0f) * 127.5f + 127.5f));
+	const int g = static_cast<int>(std::round(std::cos(I::GlobalVars->realtime + t + 2.0f) * 127.5f + 127.5f));
+	const int b = static_cast<int>(std::round(std::cos(I::GlobalVars->realtime + t + 4.0f) * 127.5f + 127.5f));
 
 	return Color_t{ static_cast<byte>(r), static_cast<byte>(g), static_cast<byte>(b), 255 };
 }
 
 Color_t CVisualUtils::RainbowTickOffset(int nTick)
 {
-	float t = TICKS_TO_TIME(nTick);
+	const float t = TICKS_TO_TIME(nTick);
 
-	int r = static_cast<int>(std::lround(std::cos((I::GlobalVars->realtime * 2.0f) + t + 0.0f) * 127.5f + 127.5f));
-	int g = static_cast<int>(std::lround(std::cos((I::GlobalVars->realtime * 2.0f) + t + 2.0f) * 127.5f + 127.5f));
-	int b = static_cast<int>(std::lround(std::cos((I::GlobalVars->realtime * 2.0f) + t + 4.0f) * 127.5f + 127.5f));
+	const int r = static_cast<int>(std::lround(std::cos((I::GlobalVars->realtime * 2.0f) + t + 0.0f) * 127.5f + 127.5f));
+	const int g = static_cast<int>(std::lround(std::cos((I::GlobalVars->realtime * 2.0f) + t + 2.0f) * 127.5f + 127.5f));
+	const int b = static_cast<int>(std::lround(std::cos((I::GlobalVars->realtime * 2.0f) + t + 4.0f) * 127.5f + 127.5f));
 
 	return Color_t{ static_cast<byte>(r), static_cast<byte>(g), static_cast<byte>(b), 255 };
 }
--- SEOwnedDE/SEOwnedDE/src/App/Features/VisualUtils/VisualUtils.h
+++ SEOwnedDE/SEOwnedDE/src/App/Features/VisualUtils/VisualUtils.h
@@ -5,23 +5,23 @@
 class CVisualUtils
 {
 public:
-	bool IsEntityOwnedBy(C_BaseEntity *pEntity, C_BaseEntity *pWho);
+	bool IsEntityOwnedBy(C_BaseEntity* pEntity, C_BaseEntity* pWho);
 
-	Color_t GetEntityColor(C_TFPlayer *pLocal, C_BaseEntity *pEntity);
+	Color_t GetEntityColor(C_TFPlayer* pLocal, C_BaseEntity* pEntity);
 	Color_t GetHealthColor(int nHealth, int nMaxHealth);
 	Color_t GetHealthColorAlt(int nHealth, int nMaxHealth);
 
-	int CreateTextureFromArray(const unsigned char *rgba, int w, int h);
-	int CreateTextureFromVTF(const char *name);
+	int CreateTextureFromArray(const unsigned char* rgba, int w, int h);
+	int CreateTextureFromVTF(const char* name);
 
 	int GetClassIcon(int nClassNum);
-	int GetBuildingTextureId(C_BaseObject *pObject);
+	int GetBuildingTextureId(C_BaseObject* pObject);
 	int GetHealthIconTextureId();
 	int GetAmmoIconTextureId();
 	int GetHalloweenGiftTextureId();
 
-	bool IsOnScreen(C_TFPlayer *pLocal, C_BaseEntity *pEntity);
-	bool IsOnScreenNoEntity(C_TFPlayer *pLocal, const Vec3 &vAbsOrigin);
+	bool IsOnScreen(const C_TFPlayer* pLocal, const C_BaseEntity* pEntity);
+	bool IsOnScreenNoEntity(const C_TFPlayer* pLocal, const Vec3& vAbsOrigin);
 
 	int GetCat(int nFrame);
 	int GetCat2(int nFrame);
