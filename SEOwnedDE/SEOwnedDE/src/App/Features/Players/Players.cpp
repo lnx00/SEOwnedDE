ccc15fbfa09ba433b41a07d1027a0964dd6a19e6
lnx00 <34384416+lnx00@users.noreply.github.com>
2024-01-01 01:59:54 +0100

    Minor player manager improvements

--- SEOwnedDE/SEOwnedDE/src/App/Features/Players/Players.cpp
+++ SEOwnedDE/SEOwnedDE/src/App/Features/Players/Players.cpp
@@ -2,6 +2,7 @@
 
 void CPlayers::Parse()
 {
+	// Init player data file path
 	if (m_LogPath.empty())
 	{
 		m_LogPath = U::Storage->GetWorkFolder() / "players.json";
@@ -24,30 +25,25 @@ void CPlayers::Parse()
 		return;
 	}
 
-	std::ifstream file(m_LogPath);
-
-	if (!file.is_open() || file.peek() == std::ifstream::traits_type::eof())
+	// Open the file
+	std::ifstream logFile(m_LogPath);
+	if (!logFile.is_open() || logFile.peek() == std::ifstream::traits_type::eof())
 	{
 		return;
 	}
 
-	nlohmann::json j{};
-
-	file >> j;
-
+	// Load all players
+	nlohmann::json j = nlohmann::json::parse(logFile);
 	for (const auto& item : j.items())
 	{
-		Player p
-		{
+		m_Players.emplace_back(
 			HASH_RT(item.key().c_str()),
-			{
+			PlayerPriority{
 				j[item.key()]["ignored"].get<bool>(),
 				j[item.key()]["cheater"].get<bool>(),
 				j[item.key()]["retardlegit"].get<bool>()
 			}
-		};
-
-		m_Players.push_back(p);
+		);
 	}
 }
 
@@ -59,41 +55,36 @@ void CPlayers::Mark(int entindex, const PlayerPriority& info)
 	}
 
 	player_info_t playerInfo{};
-
 	if (!I::EngineClient->GetPlayerInfo(entindex, &playerInfo) || playerInfo.fakeplayer)
 	{
 		return;
 	}
 
 	Player* ptr{};
-
-	auto steam_id{HASH_RT(std::string_view(playerInfo.guid).data())};
+	auto steamID = HASH_RT(std::string_view(playerInfo.guid).data());
 
 	for (auto& pl : m_Players)
 	{
-		if (pl.SteamID != steam_id)
+		if (pl.SteamID != steamID)
 		{
 			continue;
 		}
 
 		ptr = &pl;
-
 		break;
 	}
 
 	if (!ptr)
 	{
-		m_Players.push_back({steam_id, info});
-
+		m_Players.push_back({ steamID, info });
 		ptr = &m_Players.back();
 	}
 
 	ptr->Info = info;
 
+	// Load the current playerlist
 	nlohmann::json j{};
-
 	std::ifstream readFile(m_LogPath);
-
 	if (readFile.is_open() && readFile.peek() != std::ifstream::traits_type::eof())
 	{
 		readFile >> j;
@@ -101,27 +92,27 @@ void CPlayers::Mark(int entindex, const PlayerPriority& info)
 
 	readFile.close();
 
-	std::ofstream file(m_LogPath);
-
-	if (!file.is_open())
+	// Open the output file
+	std::ofstream outFile(m_LogPath);
+	if (!outFile.is_open())
 	{
 		return;
 	}
 
-	auto key{std::string(playerInfo.guid)};
+	//auto key = std::string(playerInfo.guid);
+	auto& playerEntry = j[playerInfo.guid];
 
-	j[key]["ignored"] = ptr->Info.Ignored;
-	j[key]["cheater"] = ptr->Info.Cheater;
-	j[key]["retardlegit"] = ptr->Info.RetardLegit;
+	playerEntry["ignored"] = ptr->Info.Ignored;
+	playerEntry["cheater"] = ptr->Info.Cheater;
+	playerEntry["retardlegit"] = ptr->Info.RetardLegit;
 
 	if (!ptr->Info.Ignored && !ptr->Info.Cheater && !ptr->Info.RetardLegit)
 	{
 		j.erase(std::string(playerInfo.guid));
 	}
 
-	file << std::setw(4) << j;
-
-	file.close();
+	outFile << std::setw(4) << j;
+	outFile.close();
 }
 
 bool CPlayers::GetInfo(int entindex, PlayerPriority& out)
@@ -132,14 +123,12 @@ bool CPlayers::GetInfo(int entindex, PlayerPriority& out)
 	}
 
 	player_info_t playerInfo{};
-
 	if (!I::EngineClient->GetPlayerInfo(entindex, &playerInfo) || playerInfo.fakeplayer)
 	{
 		return false;
 	}
 
-	const auto steamID{HASH_RT(std::string_view(playerInfo.guid).data())};
-
+	const auto steamID = HASH_RT(std::string_view(playerInfo.guid).data());
 	for (const auto& pl : m_Players)
 	{
 		if (pl.SteamID != steamID)
@@ -148,7 +137,6 @@ bool CPlayers::GetInfo(int entindex, PlayerPriority& out)
 		}
 
 		out = pl.Info;
-
 		return true;
 	}
 
@@ -157,8 +145,7 @@ bool CPlayers::GetInfo(int entindex, PlayerPriority& out)
 
 bool CPlayers::GetInfoGUID(const std::string& guid, PlayerPriority& out)
 {
-	const auto steamID{HASH_RT(guid.c_str())};
-
+	const auto steamID = HASH_RT(guid.c_str());
 	for (const auto& pl : m_Players)
 	{
 		if (pl.SteamID != steamID)
@@ -167,7 +154,6 @@ bool CPlayers::GetInfoGUID(const std::string& guid, PlayerPriority& out)
 		}
 
 		out = pl.Info;
-
 		return true;
 	}
 
