3b7d46620437110de0c359ca026ec4ca7439d97f
lnx00 <34384416+lnx00@users.noreply.github.com>
2024-01-04 00:11:51 +0100

    Crits: Moved lambdas to functions

--- SEOwnedDE/SEOwnedDE/src/App/Features/Crits/Crits.cpp
+++ SEOwnedDE/SEOwnedDE/src/App/Features/Crits/Crits.cpp
@@ -2,6 +2,60 @@
 
 #include "../CFG.h"
 
+bool IsFiring(const CUserCmd* pCmd, C_TFWeaponBase* weapon)
+{
+	if (weapon->GetSlot() != 2)
+	{
+		if (!weapon->HasPrimaryAmmoForShot())
+			return false;
+
+		const int nWeaponID = weapon->GetWeaponID();
+
+		if (nWeaponID == TF_WEAPON_PIPEBOMBLAUNCHER || nWeaponID == TF_WEAPON_CANNON)
+			return (G::nOldButtons & IN_ATTACK) && !(pCmd->buttons & IN_ATTACK);
+
+		if (weapon->m_iItemDefinitionIndex() == Soldier_m_TheBeggarsBazooka)
+			return G::bCanPrimaryAttack;
+	}
+
+	return (pCmd->buttons & IN_ATTACK) && G::bCanPrimaryAttack;
+}
+
+int FindCritCmd(const CUserCmd* pCmd, C_TFWeaponBase* pWeapon, bool bCrit)
+{
+	const int nBackupRandomSeed = *I::RandomSeed;
+	int nCommandNumber = pCmd->command_number;
+	int nCritCommand = nCommandNumber;
+
+	for (int n = 0; n < 4096; n++)
+	{
+		*I::RandomSeed = MD5_PseudoRandom(nCommandNumber) & 0x7FFFFFFF;
+
+		bool bCalc = false;
+
+		if (pWeapon->GetSlot() == 2)
+		{
+			bCalc = pWeapon->CalcIsAttackCriticalHelperMelee();
+		}
+		else
+		{
+			bCalc = pWeapon->CalcIsAttackCriticalHelper();
+		}
+
+		if (bCrit ? bCalc : !bCalc)
+		{
+			nCritCommand = nCommandNumber;
+			break;
+		}
+
+		nCommandNumber++;
+	}
+
+	*I::RandomSeed = nBackupRandomSeed;
+
+	return nCritCommand;
+}
+
 void CCrits::Run(CUserCmd* pCmd)
 {
 	const auto pLocal = H::Entities->GetLocal();
@@ -9,71 +63,21 @@ void CCrits::Run(CUserCmd* pCmd)
 	if (!pLocal || pLocal->deadflag() || pLocal->IsCritBoosted() || pLocal->IsMiniCritBoosted())
 		return;
 
-	static auto tf_weapon_criticals{I::CVar->FindVar("tf_weapon_criticals")};
+	static auto tf_weapon_criticals{ I::CVar->FindVar("tf_weapon_criticals") };
 
 	if (!tf_weapon_criticals->GetInt())
 	{
 		return;
 	}
 
-	auto isFiring = [&](C_TFWeaponBase* weapon)
-	{
-		if (weapon->GetSlot() != 2)
-		{
-			if (!weapon->HasPrimaryAmmoForShot())
-				return false;
-
-			const int nWeaponID = weapon->GetWeaponID();
-
-			if (nWeaponID == TF_WEAPON_PIPEBOMBLAUNCHER || nWeaponID == TF_WEAPON_CANNON)
-				return (G::nOldButtons & IN_ATTACK) && !(pCmd->buttons & IN_ATTACK);
-
-			if (weapon->m_iItemDefinitionIndex() == Soldier_m_TheBeggarsBazooka)
-				return G::bCanPrimaryAttack;
-		}
-
-		return (pCmd->buttons & IN_ATTACK) && G::bCanPrimaryAttack;
-	};
-
 	const auto pWeapon = H::Entities->GetWeapon();
 
-	if (!pWeapon || pWeapon->GetWeaponID() == TF_WEAPON_KNIFE || !isFiring(pWeapon))
+	if (!pWeapon || pWeapon->GetWeaponID() == TF_WEAPON_KNIFE || !IsFiring(pCmd, pWeapon))
 		return;
-
-	auto FindCritCmd = [&](bool bCrit)
-	{
-		int nCommandNumber = pCmd->command_number;
-		int nBackupRandomSeed = *I::RandomSeed;
-		int nCritCommand = nCommandNumber;
-
-		for (int n = 0; n < 4096; n++)
-		{
-			*I::RandomSeed = MD5_PseudoRandom(nCommandNumber) & 0x7FFFFFFF;
-
-			bool bCalc = false;
-
-			if (pWeapon->GetSlot() == 2)
-				bCalc = pWeapon->CalcIsAttackCriticalHelperMelee();
-
-			else bCalc = pWeapon->CalcIsAttackCriticalHelper();
-
-			if (bCrit ? bCalc : !bCalc)
-			{
-				nCritCommand = nCommandNumber;
-				break;
-			}
-
-			nCommandNumber++;
-		}
-
-		*I::RandomSeed = nBackupRandomSeed;
-
-		return nCritCommand;
-	};
 
 	if (pWeapon->GetSlot() == 2)
 	{
-		static auto tf_weapon_criticals_melee{I::CVar->FindVar("tf_weapon_criticals_melee")};
+		static auto tf_weapon_criticals_melee{ I::CVar->FindVar("tf_weapon_criticals_melee") };
 
 		if (!tf_weapon_criticals_melee->GetInt())
 		{
@@ -82,11 +86,11 @@ void CCrits::Run(CUserCmd* pCmd)
 
 		if (H::Input->IsDown(CFG::Exploits_Crits_Force_Crit_Key_Melee))
 		{
-			bool wantCrit{true};
+			bool wantCrit{ true };
 
 			if (G::nTargetIndex)
 			{
-				const auto ent{I::ClientEntityList->GetClientEntity(G::nTargetIndex)};
+				const auto ent{ I::ClientEntityList->GetClientEntity(G::nTargetIndex) };
 
 				if (ent && ent->GetClassId() == ETFClassIds::CTFPlayer && ent->As<C_TFPlayer>()->m_iTeamNum() == pLocal->m_iTeamNum())
 				{
@@ -96,14 +100,14 @@ void CCrits::Run(CUserCmd* pCmd)
 
 			if (wantCrit)
 			{
-				pCmd->command_number = FindCritCmd(true);
+				pCmd->command_number = FindCritCmd(pCmd, pWeapon, true);
 			}
 
 			else
 			{
 				if (CFG::Exploits_Crits_Skip_Random_Crits)
 				{
-					pCmd->command_number = FindCritCmd(false);
+					pCmd->command_number = FindCritCmd(pCmd, pWeapon, false);
 				}
 			}
 		}
@@ -112,7 +116,7 @@ void CCrits::Run(CUserCmd* pCmd)
 		{
 			if (CFG::Exploits_Crits_Skip_Random_Crits)
 			{
-				pCmd->command_number = FindCritCmd(false);
+				pCmd->command_number = FindCritCmd(pCmd, pWeapon, false);
 			}
 		}
 	}
@@ -121,14 +125,14 @@ void CCrits::Run(CUserCmd* pCmd)
 	{
 		if (H::Input->IsDown(CFG::Exploits_Crits_Force_Crit_Key))
 		{
-			pCmd->command_number = FindCritCmd(true);
+			pCmd->command_number = FindCritCmd(pCmd, pWeapon, true);
 		}
 
 		else
 		{
 			if (CFG::Exploits_Crits_Skip_Random_Crits)
 			{
-				pCmd->command_number = FindCritCmd(false);
+				pCmd->command_number = FindCritCmd(pCmd, pWeapon, false);
 			}
 		}
 	}
