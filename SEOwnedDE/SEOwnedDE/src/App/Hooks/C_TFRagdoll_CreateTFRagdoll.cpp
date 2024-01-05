b8d7f3cfe6ff4290c856f2611125b3e6b1f330fd
lnx00 <34384416+lnx00@users.noreply.github.com>
2024-01-05 01:09:45 +0100

    wtf?

--- SEOwnedDE/SEOwnedDE/src/App/Hooks/C_TFRagdoll_CreateTFRagdoll.cpp
+++ SEOwnedDE/SEOwnedDE/src/App/Hooks/C_TFRagdoll_CreateTFRagdoll.cpp
@@ -4,93 +4,35 @@
 
 MAKE_HOOK(
 	C_TFRagdoll_CreateTFRagdoll, Memory::RelToAbs(Signatures::C_TFRagdoll_CreateTFRagdoll.Get()),
-	void, __fastcall, void *ecx, void *edx)
+	void, __fastcall, C_TFRagdoll* ecx, void* edx)
 {
 	if (CFG::Visuals_Disable_Ragdolls)
 		return;
 
 	if (CFG::Visuals_Ragdolls_Active)
 	{
-		if (auto pRagdoll = reinterpret_cast<C_TFRagdoll *>(ecx))
+		if (const auto pRagdoll = ecx)
 		{
-			auto SetEffect = [&](bool bBurning, bool bElectrocuted, bool bAsh, bool bGold, bool bIce, bool bDissolve)
-			{
-				if (bBurning)
-				{
-					pRagdoll->m_bBurning() = true;
-					pRagdoll->m_bElectrocuted() = false;
-					pRagdoll->m_bBecomeAsh() = false;
-					pRagdoll->m_bGoldRagdoll() = false;
-					pRagdoll->m_bIceRagdoll() = false;
-				}
-
-				if (bElectrocuted)
-				{
-					pRagdoll->m_bBurning() = false;
-					pRagdoll->m_bElectrocuted() = true;
-					pRagdoll->m_bBecomeAsh() = false;
-					pRagdoll->m_bGoldRagdoll() = false;
-					pRagdoll->m_bIceRagdoll() = false;
-				}
-
-				if (bAsh)
-				{
-					pRagdoll->m_bBurning() = false;
-					pRagdoll->m_bElectrocuted() = false;
-					pRagdoll->m_bBecomeAsh() = true;
-					pRagdoll->m_bGoldRagdoll() = false;
-					pRagdoll->m_bIceRagdoll() = false;
-				}
-
-				if (bGold)
-				{
-					pRagdoll->m_bBurning() = false;
-					pRagdoll->m_bElectrocuted() = false;
-					pRagdoll->m_bBecomeAsh() = false;
-					pRagdoll->m_bGoldRagdoll() = true;
-					pRagdoll->m_bIceRagdoll() = false;
-				}
-
-				if (bIce)
-				{
-					pRagdoll->m_bBurning() = false;
-					pRagdoll->m_bElectrocuted() = false;
-					pRagdoll->m_bBecomeAsh() = false;
-					pRagdoll->m_bGoldRagdoll() = false;
-					pRagdoll->m_bIceRagdoll() = true;
-				}
-
-				if (bDissolve)
-				{
-					pRagdoll->m_bBurning() = false;
-					pRagdoll->m_bElectrocuted() = false;
-					pRagdoll->m_bBecomeAsh() = false;
-					pRagdoll->m_bGoldRagdoll() = false;
-					pRagdoll->m_bIceRagdoll() = false;
-					pRagdoll->m_bDissolving() = true;
-				}
-			};
-
 			int nEffect = CFG::Visuals_Ragdolls_Effect;
 
 			if (nEffect == 7)
-				nEffect = SDKUtils::RandomInt(0, 6);
-
-			switch (nEffect)
 			{
-				case 1: SetEffect(true, false, false, false, false, false); break;
-				case 2: SetEffect(false, true, false, false, false, false); break;
-				case 3: SetEffect(false, false, true, false, false, false); break;
-				case 4: SetEffect(false, false, false, true, false, false); break;
-				case 5: SetEffect(false, false, false, false, true, false); break;
-				case 6: SetEffect(false, false, false, false, false, true); break;
-				default: break;
+				nEffect = SDKUtils::RandomInt(0, 6);
 			}
+
+			pRagdoll->m_bBurning() = nEffect == 1;
+			pRagdoll->m_bElectrocuted() = nEffect == 2;
+			pRagdoll->m_bBecomeAsh() = nEffect == 3;
+			pRagdoll->m_bGoldRagdoll() = nEffect == 4;
+			pRagdoll->m_bIceRagdoll() = nEffect == 5;
+			pRagdoll->m_bDissolving() = nEffect == 6;
 
 			pRagdoll->m_vecForce() *= CFG::Visuals_Ragdolls_Force_Mult;
 
 			if (CFG::Visuals_Ragdolls_No_Death_Anim)
+			{
 				pRagdoll->m_iDamageCustom() = 0;
+			}
 		}
 	}
 
